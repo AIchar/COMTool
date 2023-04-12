96aecc57c025dd6c86fffc3ce79dd1607497c1c6
arzhe_Rm <arzhe@qq.com>
2023-04-12 19:04:01 +0800

    修改默认插件

--- COMTool/plugins/__init__.py
+++ COMTool/plugins/__init__.py
@@ -1,10 +1,10 @@
 from . import dbg
 from . import protocol
 from .import terminal
-from . import graph
-# from . import myplugin
+# from . import graph
+from . import jpgDec
 
-pluginClasses = [dbg.Plugin, protocol.Plugin, terminal.Plugin]
+pluginClasses = [dbg.Plugin, protocol.Plugin, terminal.Plugin, jpgDec.Plugin]
 # pluginClasses.append(myplugin.Plugin)
 
 builtinPlugins = {}
